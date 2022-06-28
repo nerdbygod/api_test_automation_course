66cf91ebc07c72d4b6a6f3e3565d9232a23173cf
nerdbygod <pavelartemov53@gmail.com>
2022-06-28 13:21:46 +0500

    added payload

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ BASE_URL = 'https://playground.learnqa.ru/'
 API_GET_TEXT = BASE_URL + 'api/get_text'
 API_HELLO = BASE_URL + 'api/hello'
 
-response = requests.get(API_HELLO)
+payload = {"name": "Pavel"}
+
+response = requests.get(API_HELLO, params=payload)
 print(response.text)
 
 response2 = requests.get(API_GET_TEXT)
