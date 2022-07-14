e3cc6678b4d4e4a874ab86fda007cbe2afdb85a1
nerdbygod <pavelartemov53@gmail.com>
2022-07-14 19:07:47 +0500

    added lib module with base case methods and assertions (tbd)

---
+++ lib/base_case.py
@@ -0,0 +1,30 @@
+from requests import Response
+from json.decoder import JSONDecodeError
+
+
+class BaseCase:
+    def get_cookie(
+            self, response: Response,
+            cookie_name: str
+    ):
+        assert cookie_name in response.cookies, f"Cannot find {cookie_name} in the response"
+        return response.cookies[cookie_name]
+
+    def get_header(
+            self,
+            response: Response,
+            header_name: str
+    ):
+        assert header_name in response.headers, f"Cannot find {header_name} in the response"
+        return response.headers[header_name]
+
+    def get_json_value(
+            self,
+            response: Response,
+            name: int | str
+    ):
+        try:
+            assert name in response.json(), f"There is no field with the name {name} in the response body"
+        except JSONDecodeError:
+            return f"Response is not in JSON format. Response text is {response.text}"
+        return response.json()[name]
