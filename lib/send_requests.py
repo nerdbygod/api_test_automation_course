c583b654a4dbddd996d7f0412c99eabc1f8cd031
nerdbygod <pavelartemov53@gmail.com>
2022-07-21 15:01:50 +0500

    added logging parameter to SendRequest _send function and send methods, disabled logging by default

--- lib/send_requests.py
+++ lib/send_requests.py
@@ -3,28 +3,33 @@ from .logger import Logger
 
 
 class SendRequest:
+    # To enable logs, set logging parameter to True
+    # This needs to be refactored to one single parameter passed in one function (_send),
+    # so that it is not passed every time to each method, or better use it as a pytest fixture or
+    # to pytest.ini to be passed as a command line argument
     @staticmethod
-    def post(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
-        return SendRequest._send("POST", url, data, headers, cookies)
+    def post(url: str, data: dict = None, headers: dict = None, cookies: dict = None, logging=False):
+        return SendRequest._send("POST", url, data, headers, cookies, logging)
 
     @staticmethod
-    def get(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
-        return SendRequest._send("GET", url, data, headers, cookies)
+    def get(url: str, data: dict = None, headers: dict = None, cookies: dict = None, logging=False):
+        return SendRequest._send("GET", url, data, headers, cookies, logging)
 
     @staticmethod
-    def put(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
-        return SendRequest._send("PUT", url, data, headers, cookies)
+    def put(url: str, data: dict = None, headers: dict = None, cookies: dict = None, logging=False):
+        return SendRequest._send("PUT", url, data, headers, cookies, logging)
 
     @staticmethod
-    def delete(url: str, data: dict = None, headers: dict = None, cookies: dict = None):
-        return SendRequest._send("DELETE", url, data, headers, cookies)
+    def delete(url: str, data: dict = None, headers: dict = None, cookies: dict = None, logging=False):
+        return SendRequest._send("DELETE", url, data, headers, cookies, logging)
 
     @staticmethod
     def _send(method: str,
               url: str,
               data: dict,
               headers: dict,
-              cookies: dict):
+              cookies: dict,
+              logging: bool):
 
         url = f"https://playground.learnqa.ru/api{url}"
 
@@ -33,14 +38,15 @@ class SendRequest:
         if cookies is None:
             cookies = {}
 
-        Logger.add_request(method, url, data, headers, cookies)
+        if logging:
+            Logger.add_request(method, url, data, headers, cookies)
 
         response = requests.request(method=method,
                                     url=url,
                                     data=data,
                                     headers=headers,
                                     cookies=cookies)
-
-        Logger.add_response(response)
+        if logging:
+            Logger.add_response(response)
 
         return response
