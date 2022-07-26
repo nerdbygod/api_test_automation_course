0993a6dd7dcd80e6bc2b841ed6712c1076fb2415
nerdbygod <pavelartemov53@gmail.com>
2022-07-27 01:02:45 +0500

    removed print from test_user_registration

--- tests/test_user_registration.py
+++ tests/test_user_registration.py
@@ -24,7 +24,6 @@ class TestUserRegistration(BaseCase):
 
     @allure.description("Tests if user is unable to register with existing email")
     def test_user_creation_with_existing_email(self):
-        print(f"{test_user_for_creation_data = }")
         existing_email = test_user_for_creation_data["email"]
         error_message = f"Users with email '{existing_email}' already exists"
 
